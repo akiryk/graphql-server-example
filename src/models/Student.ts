06147e0d2826e869dad9aeeac1caaf3c7a5cbad0
adam kiryk <akiryk@wayfair.com>
2023-11-03 18:48:29 -0400

    remove ts ignore

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 export const Student =
   mongoose.models.Student ||
   mongoose.model("Student", {
-    // @ts-ignore
     firstName: String,
     lastName: String,
     age: Number,
