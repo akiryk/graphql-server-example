96d770fcf49c158eb2373051df181dde6ccf5edf
adam kiryk <akiryk@wayfair.com>
2023-11-05 16:32:59 -0500

    remove dist

--- dist/models/Student.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export const Student = mongoose.models.Student ||
-    mongoose.model("Student", {
-        firstName: String,
-        lastName: String,
-        age: Number,
-    });
